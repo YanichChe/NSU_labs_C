fc5d81e24a05d8b71ddd3e44b21fddfd7f069f6a
yannie <ychernovskaya@gmail.com>
2022-06-19 23:40:52 +0700

    Change style lab1-1

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -2,83 +2,121 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+
+//////////////////////////////////////////
+
 #define MAX_INPUT_LENGHT 18
 #define _CRT_SECURE_NO_WARNINGS
 
-int input_len;
-int count_hsum_sample(unsigned char sample[]);
-int rabin_karp(int hsum_sample, unsigned char sample[]);
+//////////////////////////////////////////
 
-int main(void) {
+bool inputText(unsigned char* text, int hsumSample);
+void inputSample(unsigned char* sample);
+int inputLen;
+
+int countHsumSample(unsigned char sample[]);
+void printComprassions(unsigned char* sample, unsigned char* text, int ind);
+void rabinKarp(int hsumSample, unsigned char sample[]);
+
+// MAIN //////////////////////////////////
+
+int main(void) 
+{
 	unsigned char sample[MAX_INPUT_LENGHT];
-	unsigned char c;
-	input_len = 0;
-	int i = 0;
-	while ((c = getchar()) != '\n') {
-		sample[i] = c;
-		++i;
-		++input_len;
-	}
-	int hsum_sample = count_hsum_sample(sample);
-	rabin_karp(hsum_sample, sample);
+	inputSample(sample);
+	
+	int hsumSample = countHsumSample(sample);
+	rabinKarp(hsumSample, sample);
 	return EXIT_SUCCESS;
 }
 
-int count_hsum_sample(unsigned char sample[]) {
+// INPUT //////////////////////////////////
+
+void inputSample(unsigned char* sample)
+{
+	unsigned char c;
+	inputLen = 0;
+
+	while ((c = getchar()) != '\n') {
+		sample[inputLen++] = c;
+	}
+
+}
+
+bool inputText(unsigned char* text, int hsumSample)
+{
+	for (int i = 0; i < inputLen; i++) {
+
+		int intChar = getchar();
+		if (intChar == EOF) {
+			printf("%d ", hsumSample);
+			return false;
+		}
+		else {
+			text[i] = (unsigned char)intChar;
+		}
+	}
+	return true;
+}
+
+// RABIN KARP ////////////////////////////
+
+void printComprassions(unsigned char* sample, unsigned char* text, int ind)
+{
+	for (int i = 0; i < inputLen; i++) {
+		printf("%d ", ind + 1);
+		if (sample[i] != text[i]) {
+			break;
+		}
+		++ind;
+	}
+}
+
+int countHsumSample(unsigned char sample[]) 
+{
 	int sum = 0;
-	for (int i = 0; i < input_len; i++) {
+	for (int i = 0; i < inputLen; i++) {
 		sum += ((sample[i]) % 3) * pow(3, i);
 	}
 	return sum;
 }
 
-int rabin_karp(int hsum_sample, unsigned char sample[]) {
-	int factor = pow(3, input_len - 1);
-	int hsum0 = 0;
-	int int_char;
+void rabinKarp(int hsumSample, unsigned char sample[]) 
+{
+	int factor = pow(3, inputLen - 1);
+	int intChar;
+
 	unsigned char text[MAX_INPUT_LENGHT];
-	for (int i = 0; i < input_len; i++) {
-		int_char = getchar();
-		if (int_char == EOF) {
-			printf("%d ", hsum_sample);
-			return 0;
-		}
-		else {
-			text[i] = (unsigned char)int_char;
-		}
-	}
-	hsum0 = count_hsum_sample(text);
-	printf("%d ", hsum_sample);
-	int ind1 = 0;
+	if (inputText(text, hsumSample) == false) return;
+
+	int hsum0 = countHsumSample(text);
+	printf("%d ", hsumSample);
+
 	int ind = 0;
-	if (hsum0 == hsum_sample) {
-		for (int i = 0; i < input_len; i++) {
-			printf("%d ", ind1 + 1);
-			if (sample[i] != text[i]) {
-				break;
-			}
-			++ind1;
-		}
+
+	if (hsum0 == hsumSample) {
+		printComprassions(sample, text, ind);
 	}
-	while ((int_char = getchar()) != EOF) {
+
+	while ((intChar = getchar()) != EOF) {
+
 		int hsum1 = 0;
 		ind += 1;
-		for (int i = 0; i < input_len - 1;i++) {
+
+		for (int i = 0; i < inputLen - 1;i++) {
 			text[i] = text[i + 1];
 		}
-		text[input_len - 1] = (unsigned char)int_char;
-		hsum1 = (hsum0 / 3) + (((unsigned char)int_char) % 3) * factor;
-		if (hsum1 == hsum_sample) {
-			ind1 = ind;
-			for (int i = 0; i < input_len; i++) {
-				printf("%d ", ind1 + 1);
-				if (sample[i] != text[i]) {
-					break;
-				}
-				++ind1;
-			}
+
+		text[inputLen - 1] = (unsigned char)intChar;
+
+		hsum1 = (hsum0 / 3) + (((unsigned char)intChar) % 3) * factor;
+		if (hsum1 == hsumSample) {
+			ind = ind;
+			printComprassions(sample, text, ind);
 		}
+		
 		hsum0 = hsum1;
 	}
-	return 1;
+
 }
