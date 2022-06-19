409cfb62a82f9d2c2da50cad066d2288f286dab9
yannie <ychernovskaya@gmail.com>
2022-06-19 22:21:17 +0700

    Updata main.c

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -1,176 +1,205 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
+
 #define SIZE 100
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 
-int isCorrect(char num[], int sys1, int sys2);
-int corresp(char sys);
-char correspLetters(int s);
-long double toDec(char intPart[], char fractPart[], int sys, int lenIntPart, int lenFractPart);
-void fromDecToNewSystemIntPart(long long int intPart, int sys, char* resIntPart);
-void fromDecToNewSystemFractPart(long double fractPart, int sys, char* resFractPart);
+//////////////////////////////////////////
 
-int main(void) {
+bool isCorrect(char num[], int sysFrom, int sysTo);
+bool checkSystem(int sysFrom, int sysTo);
 
-	int sys1, sys2;
+int getIntLen(char *num);
+int getNumber(char symbol);
+char getLetter(int number);
+
+long double toDec(int system, char *num);
+int fromDecIntPart(long int number, int system, int *index, char *result);
+char *getResult(long double number, int system);
+
+// MAIN //////////////////////////////////
+
+int main(void)
+{
+	int sysFrom, sysTo;
 	char num[SIZE];
-	int isOkey = 1;
 
-	if (scanf("%d %d\n%13s", &sys1, &sys2, num) != 3) {
+	if (scanf("%d %d\n%13s", &sysFrom, &sysTo, num) != 3 || (isCorrect(num, sysFrom, sysTo) == false))
+	{
 		printf("bad input");
-		isOkey = 0;
+		return EXIT_SUCCESS;
 	}
 
-	if (isCorrect(num, sys1, sys2) == 0) {
-		printf("bad input");
-		isOkey = 0;
-	}
+	long double resToDec = toDec(sysFrom, num);
 
-	if (isOkey == 1) {
+	char *result = getResult(resToDec, sysTo);
+	printf("%s", result);
 
-		int i = 0;
-		int countIntPart = 0;
-		int countFractPart = 0;
-		char intPart[SIZE] = { 'z' };
-		char fractPart[SIZE] = { 'z' };
-
-		while (num[i] != '\0' && num[i] != '.') {
-			intPart[i] = num[i];
-			++countIntPart;
-			++i;
-		}
-		if (num[i] == '.') {
-			int j = 0;
-			++i;
-			while (num[i] != '\0') {
-				fractPart[j] = num[i];
-				++i;
-				++j;
-				++countFractPart;
-			}
-		}
-
-		char resIntPart[SIZE] = { '0' };
-		char resFractPart[SIZE] = { '0' };
-		long double newNum = toDec(intPart, fractPart, sys1, countIntPart - 1, countFractPart - 1);
-		fromDecToNewSystemIntPart((long long int)newNum, sys2, resIntPart);
-		long double decfractpart = newNum - (long long int)newNum;
-		fromDecToNewSystemFractPart(decfractpart, sys2, resFractPart);
-		printf("%s.%s\n", resIntPart, resFractPart);
-	
-	}
 	return EXIT_SUCCESS;
 }
-/*перевод из десятичной в новую систему счисления для целой части числа*/
-void fromDecToNewSystemFractPart(long double fractPart, int sys, char* resFractPart) {
 
-	int i = 0;
-	double long n = 1;
-	while (i < 12 && n != 0) {
-		n = fractPart * sys;
-		if (n != 0) {
-			resFractPart[i] = correspLetters(((int)n));
+// GET LENGHTS ///////////////////////////
+
+int getIntLen(char *num)
+{
+	int len = 0;
+	for (int i = 0; i < strlen(num); i++)
+	{
+		if (num[i] != '.' && num[i] != '\0')
+		{
+			len++;
 		}
-		fractPart = n - (int)n;
-		++i;
+		else
+			return len;
 	}
+
+	return len;
 }
-/*перевод из десятичной в новую систему счисления для дробной части числа*/
-void fromDecToNewSystemIntPart(long long int intPart, int sys, char* resIntPart) {
 
-	char buffer[SIZE] = { 'z' };
-	int i = 0;
+// CHECK ERRORS //////////////////////////
 
-	while (intPart >= sys) {
-		buffer[i] = correspLetters((intPart % sys));
-		intPart /= sys;
-		++i;
+bool checkSystem(int sysFrom, int sysTo)
+{
+	if (sysFrom > 16 || sysFrom < 2 || sysTo > 16 || sysTo < 2)
+	{
+		return false;
 	}
-	buffer[i] = correspLetters(intPart);
 
-	int j = 0;
-	for (; i >= 0; --i) {
-		if (buffer[i] != 'z') {
-			resIntPart[j] = buffer[i];
-			++j;
-		}
-	}
+	return true;
 }
-/*проверка на верность введенных значений*/
-int isCorrect(char num[], int sys1, int sys2) {
-	if (sys1 > 16 || sys1 < 2 || sys2>16 || sys2 < 2) {
-		return 0;
-	}
-	int isNumbersAfterPoint = 0;
-	int isOnePoint = 0;
+
+bool isCorrect(char num[], int sysFrom, int sysTo)
+{
+	if (checkSystem(sysFrom, sysTo) == false)
+		return false;
+
+	bool isNumbersAfterPoint = false;
+	int countPoints = 0;
 	int currentNumber;
 	int i = 0;
-	while (num[i] != '\0') {
-		if (num[i] != '.') {
-			if (isOnePoint == 1) {
-				isNumbersAfterPoint = 1;
-			}
-			currentNumber = corresp(num[i]);
-			if (currentNumber >= sys1) {
-				return 0;
-			}
+
+	while (num[i] != '\0'){
+
+		if (num[i] != '.'){
+
+			if (countPoints == 1)
+				isNumbersAfterPoint = true;
+
+			currentNumber = getNumber(num[i]);
+
+			if (currentNumber >= sysFrom)
+				return false;
 		}
-		else {
-			if (i == 0) {
-				return 0;
-			}
-			isOnePoint += 1;
+
+		else{
+			if (i == 0)
+				return false;
+			countPoints += 1;
 		}
+
 		i += 1;
 	}
-	if (isOnePoint > 1 || (isOnePoint == 1 && isNumbersAfterPoint == 0)){
-		return 0;
-	}
-	return 1;
+
+	if (countPoints > 1 || (countPoints == 1 && isNumbersAfterPoint == false))
+		return false;
+
+	return true;
 }
-/*соответсвия числам*/
-int corresp(char sys) {
-	if (sys >= '0' && sys <= '9') {
-		return sys - '0';
-	}
-	else if (sys >= 'a' && sys <= 'f') {
-		return sys - 'a' + 10;
-	}
-	else if (sys >= 'A' && sys <= 'F') {
-		return sys - 'A' + 10;
-	}
-	else {
+
+// LETTERS AND NUMBERS MATCHING //////////
+
+int getNumber(char symbol)
+{
+	if (symbol >= '0' && symbol <= '9')
+		return symbol - '0';
+
+	else if (symbol >= 'a' && symbol <= 'f')
+		return symbol - 'a' + 10;
+
+	else if (symbol >= 'A' && symbol <= 'F')
+		return symbol - 'A' + 10;
+
+	else
 		return 16;
-	}
 }
-/*соответствия буквам*/
-char correspLetters(int s) {
+
+char getLetter(int number)
+{
 	char letters[] = "0123456789abcdef";
-	return letters[s];
+	return letters[number];
 }
-/*перевод в десятичную систему счисления*/
-long double toDec(char intPart[], char fractPart[], int sys, int lenIntPart, int lenFractPart) {
+
+// FROM DEC ////////////////////////////////
+
+int fromDecIntPart(long int number, int system, int *index, char *result)
+{
+	if (number != 0){
+		fromDecIntPart(number / system, system, *index, result);
+		result[(*index)++] = getLetter(number % system);
+	}
+}
+
+char *getResult(long double number, int system)
+{
+	int index = 0;
+	char result[SIZE];
+
+	long int intPart = number;
+	long double doublePart = number - intPart;
+
+	fromDecIntPart(intPart, system, &index, result);
+
+	if (doublePart > 0){
+		
+		result[index++] = '.';
+
+		while (doublePart > 0){
+			doublePart *= system;
+			result[index++] = getLetter((int)doublePart);
+			doublePart -= (int)doublePart;
+		}
+		
+	}
+
+	result[index] = '\0';
+	return result;
+}
+
+// TO DEC ////////////////////////////////
+
+long double toDec(int system, char *num)
+{
 	int currentNumber;
-	long double res = 0;
-	int i = 0;
-	for (; i <= lenIntPart; ++i) {
-		if (intPart[i] == 'z')
+	long double result = 0;
+
+	int lenIntPart = getIntLen(num);
+	int lenFractPart = strlen(num) - lenIntPart - 1;
+
+	int i;
+	for (i = 0; i < lenIntPart; ++i){
+
+		if (num[i] == '\0')
 			break;
-		else {
-			currentNumber = corresp(intPart[i]);
-			res += currentNumber * pow(sys, (lenIntPart - i));
+		else{
+			currentNumber = getNumber(num[i]);
+			result += currentNumber * pow(system, (lenIntPart - i - 1));
 		}
+
 	}
-	i = 0;
-	for (; i <= lenFractPart; ++i) {
-		if (fractPart[i] == 'z')
+
+	for (int i = 1; i <= lenFractPart; ++i){
+
+		if (num[lenIntPart + i] == '\0')
 			break;
-		else {
-			currentNumber = corresp(fractPart[i]);
-			res += currentNumber * pow(sys, -(i + 1));
+
+		else{
+			currentNumber = getNumber(num[lenIntPart + i]);
+			result += currentNumber * pow(system, (-1) * i);
 		}
+
 	}
-	return res;
+	return result;
 }
