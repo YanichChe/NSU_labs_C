64fcdda6d27ed1e50d9dcca28741df751de0272b
yannie <ychernovskaya@gmail.com>
2022-06-20 00:12:07 +0700

    Change style lab3-1

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -2,52 +2,111 @@
 #include <string.h>
 #include <stdlib.h>
 #include <malloc.h>
-void quick_sort(int* array, int left_border, int right_border);
-void swap(int* array, int i, int j);
 
-int main(void) {
+//////////////////////////////////////////
+
+int *inputArray(int len);
+void printOutput(int len, int *array);
+
+void quickSort(int *array, int leftBorder, int rightBorder);
+void swap(int *array, int i, int j);
+
+// MAIN //////////////////////////////////
+
+int main(void)
+{
 	int len;
-	if (scanf("%d", &len) != 1) {
-		return 0;
+
+	if (scanf("%d", &len) != 1 || len == 0)
+	{
+		return EXIT_SUCCESS;
 	}
-	int *array;
-	if (len == 0) {return EXIT_SUCCESS;}
-	array = (int*)malloc(len*sizeof(int));
-	for (int i = 0;i < len;i++) {
-		if (scanf("%d", &array[i]) != 1) {
-			return 0;
-		}
-	}
-	quick_sort(array, 0, len - 1);
-	for (int i = 0; i < len;i++) {
-		printf("%d ", array[i]);
-	}
+
+	int *array = inputArray(len);
+
+	quickSort(array, 0, len - 1);
+
+	printOutput(len, array);
+
 	free(array);
+
 	return EXIT_SUCCESS;
 }
-void quick_sort(int* array, int left_border, int right_border) {
-	int i = left_border;
-	int j = right_border;
 
-	int pivot = array[(left_border + right_border) / 2];
-	do {
-		while (array[i] < pivot) { i++; }
-		while (array[j] > pivot) { j--; }
-		if (i <= j) {
-			if (array[i] > array[j]) {swap(array, i, j);}
+// OUTPUT /////////////////////////////////
+
+void printOutput(int len, int *array)
+{
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", array[i]);
+	}
+}
+
+// INPUT //////////////////////////////////
+
+int *inputArray(int len)
+{
+	int *array = (int *)malloc(len * sizeof(int));
+
+	for (int i = 0; i < len; i++)
+	{
+		if (scanf("%d", &array[i]) != 1)
+		{
+			return NULL;
+		}
+	}
+
+	return array;
+}
+
+// QUICK SORT /////////////////////////////
+
+void swap(int *array, int i, int j)
+{
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+void quickSort(int *array, int leftBorder, int rightBorder)
+{
+	int i = leftBorder;
+	int j = rightBorder;
+
+	int pivot = array[(leftBorder + rightBorder) / 2];
+	do
+	{
+		while (array[i] < pivot)
+		{
+			i++;
+		}
+
+		while (array[j] > pivot)
+		{
+			j--;
+		}
+
+		if (i <= j)
+		{
+
+			if (array[i] > array[j])
+			{
+				swap(array, i, j);
+			}
+
 			i++;
 			j--;
 		}
 	} while (i <= j);
-	if (left_border < j) {
-		quick_sort(array, left_border, j);
+
+	if (leftBorder < j)
+	{
+		quickSort(array, leftBorder, j);
 	}
-	if (i < right_border) {
-		quick_sort(array, i, right_border);
+
+	if (i < rightBorder)
+	{
+		quickSort(array, i, rightBorder);
 	}
-}
-void swap(int *array, int i, int j) {
-	int temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
 }
