3bdc41d6955a1da454622cf32b25b506fa134d0f
yannie <ychernovskaya@gmail.com>
2022-06-20 00:03:22 +0700

    Change style lab3-0

--- lab3-0/src/main.c
+++ lab3-0/src/main.c
@@ -2,50 +2,97 @@
 #include <string.h>
 #include <stdlib.h>
 #include <malloc.h>
-void swap(int* array, int i, int j);
-void heap_sort(int* array, int len);
-void sift(int* array, int len, int root);
-int main(void) {
+
+//////////////////////////////////////////
+
+void swap(int *array, int i, int j);
+void heap_sort(int *array, int len);
+void sift(int *array, int len, int root);
+
+int *input_array(int len);
+void print_output(int len, int *array);
+
+// MAIN //////////////////////////////////
+
+int main(void)
+{
 	int len;
-	if (scanf("%d", &len) != 1) {return 0;}
-	int* array;
-	if (len == 0) { return EXIT_SUCCESS; }
-	array = (int*)malloc(len * sizeof(int));
-	for (int i = 0; i < len; i++) {
-		if (scanf("%d", &array[i]) != 1) {
-			return 0;
-		}
-	}
+
+	if (scanf("%d", &len) != 1)
+		return 0;
+
+	if (len == 0)
+		return EXIT_SUCCESS;
+
+	int *array = input_array(len);
+
 	heap_sort(array, len);
-	for (int i = 0; i < len; i++) {
-		printf("%d ", array[i]);
-	}
+
+	print_output(len, array);
+
 	free(array);
+
 	return EXIT_SUCCESS;
 }
-void sift(int* array, int len, int root) {
+
+// INPUT //////////////////////////////////
+
+int *input_array(int len)
+{
+	int *array = (int *)malloc(len * sizeof(int));
+
+	for (int i = 0; i < len; i++){
+		if (scanf("%d", &array[i]) != 1){
+			return NULL;
+		}
+	}
+
+	return array;
+}
+// OUTPUT ////////////////////////////////
+
+void print_output(int len, int *array)
+{
+	for (int i = 0; i < len; i++){
+		printf("%d ", array[i]);
+	}
+}
+
+// HEAP SORT /////////////////////////////
+
+void sift(int *array, int len, int root)
+{
 	int largest = root;
 	int left = 2 * root + 1;
 	int right = 2 * root + 2;
+
 	if (left < len && array[left] > array[largest])
 		largest = left;
+
 	if (right < len && array[right] > array[largest])
 		largest = right;
-	if (largest != root) {
+
+	if (largest != root)
+	{
 		swap(array, largest, root);
 		sift(array, len, largest);
 	}
 }
-void heap_sort(int* array, int len) {
-	for (int i = len / 2 - 1; i >= 0; i--) {
+
+void heap_sort(int *array, int len)
+{
+	for (int i = len / 2 - 1; i >= 0; i--){
 		sift(array, len, i);
-	}	
-	for (int i = len - 1; i >= 0; i--) {
+	}
+
+	for (int i = len - 1; i >= 0; i--){
 		swap(array, 0, i);
 		sift(array, i, 0);
 	}
 }
-void swap(int* array, int i, int j) {
+
+void swap(int *array, int i, int j)
+{
 	int temp = array[i];
 	array[i] = array[j];
 	array[j] = temp;
