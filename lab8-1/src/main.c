1e6347537273cabf22bd68bef780adee435e946b
yannie <ychernovskaya@gmail.com>
2022-06-20 20:55:07 +0700

    Change style lab8-1

--- lab8-1/src/main.c
+++ lab8-1/src/main.c
@@ -2,167 +2,255 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdbool.h>
-#define MAX_NUMBER_VERTEX  5000
 
-bool check_errors(int vertices_number, int edges_number)
+//////////////////////////////////////////
+
+#define MAX_NUMBER_VERTEX 5000
+
+//////////////////////////////////////////
+
+bool checkErrors(int numberVertices, int numberEdges);
+bool checkEdge(int start, int end, int numberVertices, long long int distance);
+bool checkSpanningTree(int* parent, int numberVertices);
+
+int **createMatrix(int numberVertices);
+long long int *createMinDistance(int numberVertices);
+bool inputMatrix(int numberEdges, int numberVertices, int **matrix);
+
+void changeMinDistance(int minIndex, int **matrix,
+					   long long int *minDistance, int numberVertices, int *parent, int *visited);
+
+void setValue(long long int *minElement, int *minIndex);
+int *primAlgoritnm(long long int *minDistance, int **matrix, int index, int numberVertices);
+
+void printMST(int numberVertices, int* parent);
+
+void freeAll(long long int *minDistance, int **matrix, int numberVertices);
+
+// MAIN //////////////////////////////////
+
+int main()
 {
-	if (vertices_number < 0 || vertices_number > MAX_NUMBER_VERTEX) {
-		printf("bad number of vertices");
-		return true;
+	int numberVertices;
+	int numberEdges;
+
+	if (scanf("%d\n%d", &numberVertices, &numberEdges) != 2)
+	{
+		return EXIT_SUCCESS;
 	}
-	if (edges_number < 0 || edges_number >(vertices_number) * (vertices_number + 1) / 2) {
-		printf("bad number of edges");
-		return true;
+
+	if (!checkErrors(numberVertices, numberEdges))
+	{
+		return EXIT_SUCCESS;
 	}
-	return false;
+
+	long long int *minDistance = createMinDistance(numberVertices);
+	int **matrix = createMatrix(numberVertices);
+
+	if (!inputMatrix(numberEdges, numberVertices, matrix))
+	{
+		freeAll(minDistance, matrix, numberVertices);
+		return EXIT_SUCCESS;
+	}
+
+	int *parent = primAlgoritnm(minDistance, matrix, 1, numberVertices);
+
+	if (!checkSpanningTree(parent,numberVertices))
+	{
+		freeAll(minDistance, matrix, numberVertices);
+		free(parent);
+		return EXIT_SUCCESS;
+	}
+
+	printMST(numberVertices, parent);
+
+	freeAll(minDistance, matrix, numberVertices);
+	free(parent);
+	
+	return EXIT_SUCCESS;
 }
 
-bool check_edge(int start, int end, int vertices_number, int distance)
+// ERRORS ////////////////////////////////////////
+
+bool checkErrors(int numberVertices, int numberEdges)
 {
-	if (start < 1 || start > vertices_number || end < 1 || end > vertices_number) {
+	if (numberVertices == 0)
+	{
+		printf("no spanning tree");
+		return false;
+	}
+
+	if (numberVertices < 0 || numberVertices > MAX_NUMBER_VERTEX)
+	{
+		printf("bad number of vertices");
+		return false;
+	}
+	if (numberEdges < 0 || numberEdges > (numberVertices) * (numberVertices + 1) / 2)
+	{
+		printf("bad number of edges");
+		return false;
+	}
+	return true;
+}
+
+bool checkEdge(int start, int end, int numberVertices, long long int distance)
+{
+	if (start < 1 || start > numberVertices || end < 1 || end > numberVertices)
+	{
 		printf("bad vertex");
 		return true;
 	}
-	if (distance < 0 || distance >INT_MAX) {
+	if (distance < 0 || distance > INT_MAX)
+	{
 		printf("bad length");
 		return true;
 	}
 	return false;
 }
 
-int** create_matrix(int vertices_number)
+bool checkSpanningTree(int* parent, int numberVertices)
 {
-	int** matrix = (int**)calloc(vertices_number, sizeof(int*));
-	for (int i = 0; i < vertices_number; i++) {
-		matrix[i] = (int*)calloc(vertices_number, sizeof(int));
+	int noSpanningTree = 0;
+
+	for (int i = 0; i < numberVertices; i++)
+	{
+		if (parent[i] == 0)
+		{
+			noSpanningTree++;
+		}
+	}
+
+	if (noSpanningTree != 1)
+	{
+		printf("no spanning tree");
+		return false;
+	}
+
+	return true;
+}
+//////////////////////////////////////////
+
+int **createMatrix(int numberVertices)
+{
+	int **matrix = (int **)calloc(numberVertices, sizeof(int *));
+	for (int i = 0; i < numberVertices; i++)
+	{
+		matrix[i] = (int *)calloc(numberVertices, sizeof(int));
 	}
 	return matrix;
 }
 
-long long int* create_min_distance(int vertices_number)
+long long int *createMinDistance(int numberVertices)
 {
-	long long int* min_distance = (long long int*)malloc(vertices_number * sizeof(long long int));
-	for (int i = 0; i < vertices_number; i++) {
-		min_distance[i] = LLONG_MAX;
+	long long int *minDistance = (long long int *)malloc(numberVertices * sizeof(long long int));
+	for (int i = 0; i < numberVertices; i++)
+	{
+		minDistance[i] = LLONG_MAX;
 	}
-	return min_distance;
+	return minDistance;
 }
 
-void change_min_distance(int min_index, int** matrix, 
-	long long int* min_distance, int vertices_number, int* parent, int* visited) 
+bool inputMatrix(int numberEdges, int numberVertices, int **matrix)
 {
-	for (int i = 0; i < vertices_number; i++) {
-		if (visited[i] == 0) {
-			if (matrix[min_index][i] > 0) {
-				long long int tmp = matrix[min_index][i];
-				if (tmp < min_distance[i]) {
-					min_distance[i] = tmp;
-					parent[i] = min_index + 1;
-				}
+	for (int i = 0; i < numberEdges; i++)
+	{
+		int start, end;
+		long long distance;
+
+		if (scanf("%d %d %lld", &start, &end, &distance) != 3)
+		{
+			printf("bad number of lines");
+			return false;
+		}
+		if (checkEdge(start, end, numberVertices, distance))
+		{
+			return false;
+		}
+		matrix[start - 1][end - 1] = (int)distance;
+		matrix[end - 1][start - 1] = (int)distance;
+	}
+
+	return true;
+}
+
+//////////////////////////////////////////
+
+void changeMinDistance(int minIndex, int **matrix,
+					   long long int *minDistance, int numberVertices, int *parent, int *visited)
+{
+	for (int i = 0; i < numberVertices; i++)
+	{
+		if (visited[i] == 0 && matrix[minIndex][i] > 0)
+		{
+			long long int tmp = matrix[minIndex][i];
+			if (tmp < minDistance[i])
+			{
+				minDistance[i] = tmp;
+				parent[i] = minIndex + 1;
 			}
 		}
 	}
 }
 
-void set_value(long long int* min_element, int* min_index) {
-	*min_element = LLONG_MAX;
-	*min_index = INT_MAX;
+//////////////////////////////////////////
+
+void setValue(long long int *minElement, int *minIndex)
+{
+	*minElement = LLONG_MAX;
+	*minIndex = INT_MAX;
 }
 
-int* fill_min_distance(long long int* min_distance, int** matrix, int index, int vertices_number)
+int *primAlgoritnm(long long int *minDistance, int **matrix, int index, int numberVertices)
 {
-	int* visited = (int*)calloc(vertices_number, sizeof(int));
-	int* parent = (int*)calloc(vertices_number, sizeof(int));
+	int *visited = (int *)calloc(numberVertices, sizeof(int));
+	int *parent = (int *)calloc(numberVertices, sizeof(int));
 
-	min_distance[index - 1] = 0;
-	int min_index = 0;
-	long long int min_element = 0;
+	minDistance[index - 1] = 0;
+	int minIndex = 0;
+	long long int minElement = 0;
 
-	while (min_index != INT_MAX) {
-		set_value(&min_element, &min_index);
-		for (int i = 0; i < vertices_number; i++) {
-			if (visited[i] == 0 && min_distance[i] < min_element) {
-				min_element = min_distance[i];
-				min_index = i;
+	while (minIndex != INT_MAX)
+	{
+		setValue(&minElement, &minIndex);
+		for (int i = 0; i < numberVertices; i++)
+		{
+			if (visited[i] == 0 && minDistance[i] < minElement)
+			{
+				minElement = minDistance[i];
+				minIndex = i;
 			}
 		}
-		if (min_index != INT_MAX) {
-			change_min_distance(min_index, matrix, min_distance, vertices_number, parent, visited);
-			visited[min_index] = 1;
+		if (minIndex != INT_MAX)
+		{
+			changeMinDistance(minIndex, matrix, minDistance, numberVertices, parent, visited);
+			visited[minIndex] = 1;
 		}
 	}
 	free(visited);
 	return parent;
 }
 
+//////////////////////////////////////////
 
+void printMST(int numberVertices, int* parent)
+{
+	for (int i = 0; i < numberVertices; i++)
+	{
+		if (parent[i] != 0)
+		{
+			printf("%d %d\n", i + 1, parent[i]);
+		}
+	}
+}
 
-void free_arrays(long long int* min_distance, int** matrix, int vertices_number) {
-	free(min_distance);
-	for (int i = 0; i < vertices_number; i++) {
+//////////////////////////////////////////
+
+void freeAll(long long int *minDistance, int **matrix, int numberVertices)
+{
+	free(minDistance);
+	for (int i = 0; i < numberVertices; i++)
+	{
 		free(matrix[i]);
 	}
 	free(matrix);
 }
-
-int main()
-{
-	int vertices_number;
-	int edges_number;
-
-	if (scanf("%d\n%d", &vertices_number, &edges_number) != 2) {
-		return EXIT_SUCCESS;
-	}
-
-	if (vertices_number == 0) {
-		printf("no spanning tree");
-		return EXIT_SUCCESS;
-	}
-
-	if (check_errors(vertices_number, edges_number)) {
-		return EXIT_SUCCESS;
-	}
-
-	long long int* min_distance = create_min_distance(vertices_number);
-	int** matrix = create_matrix(vertices_number);
-
-	for (int i = 0; i < edges_number; i++) {
-		int start, end, distance;
-		if (scanf("%d %d %d", &start, &end, &distance) != 3) {
-			printf("bad number of lines");
-			free_arrays(min_distance, matrix, vertices_number);
-			return EXIT_SUCCESS;
-		}
-		if (check_edge(start, end, vertices_number, distance)) {
-			free_arrays(min_distance, matrix, vertices_number);
-			return EXIT_SUCCESS;
-		}
-		matrix[start - 1][end - 1] = distance;
-		matrix[end - 1][start - 1] = distance;
-	}
-
-	int* parent = fill_min_distance(min_distance, matrix, 1, vertices_number);
-	int no_spanning = 0;
-
-	for (int i = 0; i < vertices_number; i++) {
-		if (parent[i] == 0) {
-			no_spanning++;
-		}
-	}
-	if (no_spanning != 1) {
-		printf("no spanning tree");
-		free_arrays(min_distance, matrix, vertices_number);
-		free(parent);
-		return EXIT_SUCCESS;
-	}
-	for (int i = 0; i < vertices_number; i++) {
-		if (parent[i] != 0) {
-			printf("%d %d\n", i + 1, parent[i]);
-		}
-	}
-	free_arrays(min_distance, matrix, vertices_number);
-	free(parent);
-	return EXIT_SUCCESS;
-}
-
-
