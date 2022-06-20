e950b55f7c30b57cf38519147ad3f41daa57acca
yannie <ychernovskaya@gmail.com>
2022-06-20 11:35:01 +0700

    Update main lab7

--- lab7/src/main.c
+++ lab7/src/main.c
@@ -1,120 +1,189 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <malloc.h>
+#include <stdbool.h>
 
-#define SUCCESS 0;
-#define ERROR 1;
+//////////////////////////////////////////
 
-int topologic_sort(int ind, int visited[], int n, char** matrix, int output[], int flag, int* k);
-int fill_matrix(int n, int m, char** matrix);
-void print_reverse(int n, int output[]);
+bool inputNumberVertices(int* numberVertices);
+bool inputMatrix(int numberVertices, int numberEdges, char** matrix);
+char** createMatrix(int numberVertices);
 
-void free_arrays(char** matrix, int* visited, int* output, int n) {
-	for (int i = 0; i < n; i++) {
+bool checkNumberLines(int* numberEdges);
+bool checkNumberEdges(int numberEdges, int numberVertices);
+bool checkVertex(int start, int finish, int numberVertices);
+
+bool DFS(int numberVertices, int* visited, char** matrix, int* output);
+int topologicSort(int index, int visited[], int numberVertices, char** matrix, int output[], int flag, int* indexOutput);
+void printReverse(int numberVertices, int output[]);
+
+//////////////////////////////////////////
+
+void freeArrays(char** matrix, int* visited, int* output, int numberVertices) 
+{
+	for (int i = 0; i < numberVertices; i++) {
 		free(matrix[i]);
 	}
 	free(matrix);
 	free(visited);
 	free(output);
 }
-int main() {
-	int n = 0;
-	char** matrix;
-	int* visited;
-	int* output;
-	if (scanf("%d", &n) != 1) {
-		printf("bad number of lines");
-		return EXIT_SUCCESS;
-	}
-	if (n < 0 || n > 2000) {
-		printf("bad number of vertices");
+
+// MAIN //////////////////////////////////
+
+int main() 
+{
+	int numberVertices = 0;
+
+	if (!inputNumberVertices(&numberVertices)){
 		return EXIT_SUCCESS;
 	}
 
-	output = (int*)malloc(sizeof(int) * n);
-	matrix = (char**)malloc(sizeof(char*) * n);
-	int z = (n - n % 8 + 8) / 8;
-	for (int i = 0; i < n; i++) {
-		matrix[i] = (char*)malloc(sizeof(char) * z);
-		for (int j = 0; j < z; ++j) {
+	int* output = (int*)malloc(sizeof(int) * numberVertices);
+	char** matrix = createMatrix(numberVertices);
+	int* visited = calloc(numberVertices, sizeof(int));
+
+	int numberEdges = 0;
+
+	if (!checkNumberLines(&numberEdges) || !checkNumberEdges(numberEdges, numberVertices)
+		|| !inputMatrix(numberVertices, numberEdges, matrix)) {
+
+		freeArrays(matrix, visited, output, numberVertices);
+		return EXIT_SUCCESS;
+
+	}
+
+	DFS(numberVertices, visited, matrix, output);
+	printReverse(numberVertices, output);
+
+	freeArrays(matrix, visited, output, numberVertices);
+
+	return EXIT_SUCCESS;
+}
+
+// CHECK ERRORS /////////////////////////
+
+bool checkNumberLines(int* numberEdges)
+{
+	if (scanf("%d", numberEdges) != 1) {
+		printf("bad number of lines");
+		return false;
+	}
+
+	return true;
+}
+
+bool checkNumberEdges(int numberEdges, int numberVertices)
+{
+	if (numberEdges < 0 || numberEdges > numberVertices * (numberVertices + 1) / 2) {
+		printf("bad number of edges");
+		return false;
+	}
+
+	return true;
+}
+
+bool checkVertex(int start, int finish, int numberVertices)
+{
+	if (start <= 0 || finish <= 0 || start > numberVertices || finish > numberVertices) {
+		printf("bad vertex");
+		return false;
+	}
+
+	return true;
+}
+
+//////////////////////////////////////////
+
+char** createMatrix(int numberVertices)
+{
+	char** matrix = (char**)malloc(sizeof(char*) * numberVertices);
+
+	int len = (numberVertices - numberVertices % 8 + 8) / 8;
+	for (int i = 0; i < numberVertices; i++) {
+		matrix[i] = (char*)malloc(sizeof(char) * len);
+		for (int j = 0; j < len; ++j) {
 			matrix[i][j] = 0;
 		}
 	}
 
-	visited = (int*)malloc(n * sizeof(int));
-	for (int i = 0; i < n; i++) {
-		visited[i] = 0;
-	}
-	int m = 0;
+	return matrix;
+}
 
-	if (scanf("%d", &m) != 1) {
-		printf("bad number of lines");
-		free_arrays(matrix, visited, output, n);
-		return EXIT_SUCCESS;
-	}
+// TOPOLOGIC SORT /////////////////////////
 
-	if (m < 0 || m > n * (n + 1) / 2) {
-		printf("bad number of edges");
-		free_arrays(matrix, visited, output, n);
-		return EXIT_SUCCESS;
-	}
+bool DFS(int numberVertices, int* visited, char** matrix, int* output)
+{
+	int indexOutput = 0;
 
-	if (fill_matrix(n, m, matrix) == 1) {
-		free_arrays(matrix, visited, output, n);
-		return EXIT_SUCCESS;
-	}
-	int k = 0;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < numberVertices; i++) {
 		if (visited[i] == 0) {
-			if (topologic_sort(i, visited, n, matrix, output, i, &k) == 1) {
+			if (!topologicSort(i, visited, numberVertices, matrix, output, i, &indexOutput)) {
 				printf("impossible to sort");
-				free_arrays(matrix, visited, output, n);
-				return EXIT_SUCCESS;
+				return false;
 			}
 		}
 	}
-	print_reverse(n, output);
-	free_arrays(matrix, visited, output, n);
-	return EXIT_SUCCESS;
+
+	return true;
 }
-int topologic_sort(int ind, int visited[], int n, char** matrix, int output[], int flag, int* k) {
-	visited[ind] = 1;
-	for (int i = n - 1; i >= 0; i--) {
-		int el = matrix[ind][i / 8];
+
+int topologicSort(int index, int visited[], int numberVertices, char** matrix, int output[], int flag, int* indexOutput) 
+{
+	visited[index] = 1;
+
+	for (int i = numberVertices - 1; i >= 0; i--) {
+		int el = matrix[index][i / 8];
 		if ((el & (1 << (7 - i % 8))) != 0 && (visited[i] == 0))
-			if (topologic_sort(i, visited, n, matrix, output, flag, k) == 1) {
-				return ERROR;
+			if (!topologicSort(i, visited, numberVertices, matrix, output, flag, indexOutput)) {
+				return false;
 			}
 		if ((el & (1 << (7 - i % 8))) != 0 && (visited[i] == 1) && (i == flag))
-			return ERROR;
+			return false;
 	}
-	output[(*k)++] = ind + 1;
-	return SUCCESS;
+
+	output[(*indexOutput)++] = index + 1;
+	return true;
 }
 
-int fill_matrix(int n, int m, char** matrix) {
-	for (int i = 0; i < m; i++) {
-		int a, b;
-		if (scanf("%d %d", &a, &b) != 2) {
-			printf("bad number of lines");
-			return ERROR;
-		}
-		if (a <= 0 || b <= 0 || a > n || b > n) {
-			printf("bad vertex");
-			return ERROR;
-		}
-		if (a == b) {
-			printf("impossible to sort");
-			return ERROR;
-		}
-		int el = matrix[a - 1][(b - 1) / 8];
-		matrix[a - 1][(b - 1) / 8] = el | (1 << (7 - ((b - 1) % 8)));
-	}
-	return SUCCESS;
-}
-
-void print_reverse(int n, int output[]) {
-	for (int i = n - 1; i >= 0; i--) {
+void printReverse(int numberVertices, int output[]) 
+{
+	for (int i = numberVertices - 1; i >= 0; i--) {
 		printf("%d ", output[i]);
 	}
+}
+
+// INPUT /////////////////////////////////
+
+bool inputNumberVertices(int* numberVertices)
+{
+	if (scanf("%d", numberVertices) != 1) {
+		printf("bad number of lines");
+		return false;
+	}
+	if (*numberVertices < 0 || *numberVertices > 2000) {
+		printf("bad number of vertices");
+		return false;
+	}
+
+	return true;
+}
+
+bool inputMatrix(int numberVertices, int numberEdges, char** matrix) 
+{
+	for (int i = 0; i < numberEdges; i++) {
+		int start, finish;
+		if (scanf("%d %d", &start, &finish) != 2) {
+			printf("bad number of lines");
+			return false;
+		}
+		if (!checkVertex(start, finish, numberVertices)) return false;
+		if (start == finish) {
+			printf("impossible to sort");
+			return false;
+		}
+		int el = matrix[start - 1][(finish - 1) / 8];
+		matrix[start - 1][(finish - 1) / 8] = el | (1 << (7 - ((finish - 1) % 8)));
+	}
+	return true;
 }
